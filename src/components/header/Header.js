ee6e1c3844e832e5a68ccc53d4795c8d37e43d09
Valery FH <valeryfigueroah@gmail.com>
2021-01-15 20:04:56 +0100

    header updated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,23 +6,18 @@ import { useHistory } from "react-router-dom";
 const Header = () => {
   const history = useHistory();
   return (
-    <div className="Header__container">
-      <div className="Logo__container">
-        <Logo />
-      </div>
+    <div className="header__container">
+      <Logo />
       <div className="buttons_container">
         <div className="PlainText__container">How it works</div>
-        <div className="LoginButton__container">
-          <Button
-            name="Login"
-            alt="Button to acces at Login"
-            onClick={() => history.push("/login")}
-          />
-        </div>
-        <div className="SignUpButton__container">
-          <Button name="Sign up" alt="Button to acces at Sign up" />{" "}
-          {/*onClick={()=> history.push("/signup")} development de signup page*/}
-        </div>
+        <Button
+          name="Login"
+          alt="Button to acces at Login"
+          onClick={() => history.push("/login")}
+          className=""
+        />
+        <Button name="Sign up" alt="Button to acces at Sign up" />{" "}
+        {/*onClick={()=> history.push("/signup")} development de signup page*/}
       </div>
     </div>
   );
