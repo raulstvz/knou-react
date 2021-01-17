3e15ac64a8d1e5f2cdcd66489ea006d291cac7b5
Franc Cosp <franccospmeyaeso@gmail.com>
2021-01-17 12:44:09 +0100

    changed to develop

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,11 +1,13 @@
 import "./Button.css"
 
-const Button = ({name,onClick, alt}) => {
+const Button = ({ name, onClick, alt, color }) => {
 
-    return(
-        <div onClick={onClick} alt = {alt} className="btn_container">
-         <div className="container">{name}</div>  
-         </div>
+
+
+    return (
+        <div onClick={onClick} alt={alt} className={color} style={{ backgroundColor: { color } == "dark" ? '#f1e4ff' : ' #8c30f5;', color: { color } == "dark" ? ' #8c30f5;' : '#f1e4ff' }}>
+            <p> {name}</p>
+        </div>
 
     )
 };
