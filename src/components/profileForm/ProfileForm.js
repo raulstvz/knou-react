bc2bc3c46e1cdf6a708918d46054d950052e2611
Valery <valeryfigueroah@gmail.com>
2021-01-20 11:29:41 +0100

    prefereces page finished

--- src/components/profileForm/ProfileForm.js
+++ src/components/profileForm/ProfileForm.js
@@ -1,105 +1,118 @@
 import { React, useState } from "react";
 import { useHistory } from "react-router";
 import "./ProfileForm.css";
-import Logo from "../../assets/logo/image 2.png";
+import Logo from "../../components/logo/Logo";
 import Button from "../button/Button";
 
 const ProfileForm = () => {
+  const history = useHistory();
 
-    const history = useHistory();
+  //formData : combo for the inputs
+  const [formData, setFormData] = useState({
+    firstname: undefined,
+    lastname: undefined,
+    birthdate: new Date(),
+    location: undefined,
+    description: undefined,
+    gender: undefined,
+    //photos
+    //hobbies
+  });
 
-    //formData : combo for the inputs
-    const [formData, setFormData] = useState({
-        firstname: undefined,
-        lastname: undefined,
-        birthdate: new Date(),
-        location: undefined,
-        description: undefined,
-        gender: undefined,
-        //photos
-        //hobbies
-    })
+  //Body
+  const body = {
+    profile: {
+      firstname: formData.firstname,
+      lastname: formData.lastname,
+      //birthdate
+      location: formData.location,
+      description: formData.description,
+      gender: formData.gender,
+    },
+  };
 
-    //Body
-    const body = {
-        profile: {
-            firstname: formData.firstname,
-            lastname: formData.lastname,
-            //birthdate
-            location: formData.location,
-            description: formData.description,
-            gender: formData.gender
-        }
+  console.log(body);
+
+  //Fetch function
+  const handleCreate = () => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
     };
 
-    console.log(body)
+    fetch("http://localhost:3001/api/users", options);
+    history.push("/preferencesform");
+  };
 
-    //Fetch function
-    const handleCreate = () => {
-        const options = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(body),
-        };
-
-        fetch("http://localhost:3001/api/users", options)
-        history.push("/preferencesform")
-    };
-
-    return (
-        <div className="signupform__container">
-            <div className="Logo__form" src={Logo} alt="logo" />
-            <h4>Create new account</h4>
-            <form>
-                <label>
-                    First Name
-              <input
-                        name="source"
-                        onChange={(e) =>
-                            setFormData({ ...formData, firstname: e.target.value })}>
-                    </input>
-                </label>
-                <label>
-                    Last Name
-              <input
-                        name="source"
-                        onChange={(e) =>
-                            setFormData({ ...formData, lastname: e.target.value })}>
-                    </input>
-                </label>
-                <label>
-                    Location
-              <input
-                        name="source"
-                        onChange={(e) =>
-                            setFormData({ ...formData, location: e.target.value })}>
-                    </input>
-                </label>
-                <label>
-                    Description
-              <input
-                        name="source"
-                        onChange={(e) =>
-                            setFormData({ ...formData, description: e.target.value })}>
-                    </input>
-                </label>
-                <label>
-                    Gender
-                    <select onChange={(e) =>
-                        setFormData({ ...formData, gender: e.target.value })}>
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="others">Others</option>
-                    </select>
-                </label>
-                <div className="createbutton__container">
-                    <Button name="Create" onClick={handleCreate} />
-                </div>
-            </form>
+  return (
+    <div className="profileForm__container">
+      <div className="Logo__form">
+        <Logo />
+      </div>
+      <h4 className="title_form">Create your profile</h4>
+      <form className="formProfile_container">
+        <label className="name_container">
+          First Name
+          <input
+            name="source"
+            className="input_form"
+            onChange={(e) =>
+              setFormData({ ...formData, firstname: e.target.value })
+            }
+          ></input>
+        </label>
+        <label className="lastName_container">
+          Last Name
+          <input
+            name="source"
+            className="input_form"
+            onChange={(e) =>
+              setFormData({ ...formData, lastname: e.target.value })
+            }
+          ></input>
+        </label>
+        <label className="location_container">
+          Location
+          <input
+            name="source"
+            className="input_form"
+            onChange={(e) =>
+              setFormData({ ...formData, location: e.target.value })
+            }
+          ></input>
+        </label>
+        <label className="description_container">
+          Description
+          <textarea
+            name="source"
+            className="input_form"
+            onChange={(e) =>
+              setFormData({ ...formData, description: e.target.value })
+            }
+          ></textarea>
+        </label>
+        <label className="gender_container">
+          Gender
+          <select
+            onChange={(e) =>
+              setFormData({ ...formData, gender: e.target.value })
+            }
+            className="input_form"
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="others">Others</option>
+          </select>
+        </label>
+        <div className="createbutton__container">
+          <Button name="Create" onClick={handleCreate} />
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default ProfileForm;
--- src/components/signupForm/SignupForm.js
+++ src/components/signupForm/SignupForm.js
@@ -1,91 +1,101 @@
 import { React, useState } from "react";
 import { useHistory } from "react-router";
 import "./SignupForm.css";
-import Logo from "../../assets/logo/image 2.png";
+import Logo from "../../components/logo/Logo";
 import Button from "../button/Button";
 
 const SignUpForm = () => {
+  const history = useHistory();
 
-    const history = useHistory();
+  //formData : combo for the inputs
+  const [formData, setFormData] = useState({
+    username: undefined,
+    email: undefined,
+    password: undefined,
+    avatar: undefined,
+  });
 
-    //formData : combo for the inputs
-    const [formData, setFormData] = useState({
-        username: undefined,
-        email: undefined,
-        password: undefined,
-        avatar: undefined
-    })
+  //Body
+  const body = {
+    username: formData.username,
+    email: formData.email,
+    password: formData.password,
+    avatar: formData.avatar,
+    usercreation: new Date(),
+    premium: false,
+  };
 
-    //Body
-    const body = {
-        username: formData.username,
-        email: formData.email,
-        password: formData.password,
-        avatar: formData.avatar,
-        usercreation: new Date(),
-        premium: false,
+  console.log(body);
+
+  //Fetch function
+  const handleCreate = () => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
     };
 
-    console.log(body)
+    fetch("http://localhost:3001/api/users", options);
+    history.push("/profileform");
+  };
 
-    //Fetch function
-    const handleCreate = () => {
-        const options = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(body),
-        };
-
-        fetch("http://localhost:3001/api/users", options)
-        history.push("/profileform")
-    };
-
-    return (
-        <div className="signupform__container">
-            <div className="Logo__form" src={Logo} alt="logo"/>
-            <h4>Create new account</h4>
-            <form>
-                <label>
-                    Username
-              <input
-                        name="source"
-                        onChange={(e) =>
-                            setFormData({ ...formData, username: e.target.value })}>
-                    </input>
-                </label>
-                <label>
-                    Email
-              <input
-                        name="source"
-                        onChange={(e) =>
-                            setFormData({ ...formData, email: e.target.value })}>
-                    </input>
-                </label>
-                <label>
-                    Password
-              <input
-                        name="source"
-                        type="password"
-                        onChange={(e) =>
-                            setFormData({ ...formData, password: e.target.value })}>
-                    </input>
-                </label>
-                <label>
-                    Avatar
-              <input
-                        name="source"
-                        onChange={(e) =>
-                            setFormData({ ...formData, avatar: e.target.value })}>
-                    </input>
-                </label>
-                <div className="createbutton__container">
-                    <Button name="Create" onClick={handleCreate} />
-                </div>
-            </form>
+  return (
+    <div className="signUpForm__contanier">
+       <div className="Logo__form">
+      <Logo/>
+      </div>
+      <h4 className="title_form">Sign up for your new account</h4>
+      <form className="formSignUp_container">
+        <label className="username_container">
+          Username
+          <input
+            name="source"
+            className="input_form"
+            onChange={(e) =>
+              setFormData({ ...formData, username: e.target.value })
+            }
+          ></input>
+        </label>
+        <label className="email_container">
+          Email
+          <input
+            name="source"
+            className="input_form"
+            type="email"
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+          ></input>
+        </label>
+        <label className="password_container">
+          Password
+          <input
+            name="source"
+            className="input_form"
+            type="password"
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+          ></input>
+        </label>
+        <label className="avatar_container">
+          Avatar
+          <input
+            name="source"
+            className="input_form"
+            onChange={(e) =>
+              setFormData({ ...formData, avatar: e.target.value })
+            }
+          ></input>
+        </label>
+        <div className="createbutton__container">
+          <Button name="Create" onClick={handleCreate} />
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default SignUpForm;
