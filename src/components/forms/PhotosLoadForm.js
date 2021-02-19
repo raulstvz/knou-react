57f0c98683e2d82a3f39464e04e1a6232d5df530
Franc Cosp Meya <franccospmeyaeso@gmail.com>
2021-02-19 21:01:37 +0100

    ja esta mes o menys

--- src/components/forms/PhotosLoadForm.js
+++ src/components/forms/PhotosLoadForm.js
@@ -10,52 +10,14 @@ const PhotosLoadForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => 
     const history = useHistory()
 
     /* Controls the photo insertion through the PhotoLoader component */
-    const [photoArray, setPhotoArray] = useState(null);
 
-
-    /* useEffect(() => {
-        setFormData({ ...formData, photos: photoArray })
-    }, [photoArray]) */
-
-    /* Data to be passed as body in the fetch */
-    const handleImageChange = (e) => {
-        setPhotoArray(e.target.files);
-    };
 
     /* Actions for the buttons in the forms */
-    const handleFinish = () => {
-        /* TODO: DEFINE PUT/POST ACTION AGAINST MONGODB */
-
-        let form_data = new FormData(); // https://developer.mozilla.org/es/docs/Web/API/XMLHttpRequest/FormData
-        form_data.append('sigup_step', currentStep + 1);
-        form_data.append('updated', new Date());
-        form_data.append('signup_completed', true);
-        Array.from(photoArray).forEach(photo => {
-            form_data.append('photos', photo, photo.name);
-        });
-        
-       
-
-        const options = {
-            method: "PUT",
-            body: form_data
-        };
-
-        fetch("http://localhost:3001/api/users/" + userId + "/photos", options)
-            .then((res) => {
-                if (res.ok) {
-                    /* history.push("/profile") */
-                }
-                else {
-                    console.log("error")
-                }
-            })
-    };
 
     const handlePrevious = () => {
         setCurrentStep(currentStep - 1)
     }
-    
+
     return (
         <div className="form">
             <Stepper steps={totalSteps} currentStep={currentStep} />
@@ -65,17 +27,9 @@ const PhotosLoadForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => 
             <br /><br />
             <p>It's show time! Pick up to 8 pictures of you:</p>
             <div className="form__photos">
-                <PhotoLoader photoArray={photoArray} />
+                <PhotoLoader userId={userId}/>
             </div>
-            <form id="photo"
-                encType="multipart/form-data"
-                className="form__container"
-                onSubmit={(e) => {
-                    e.preventDefault()
-                }}
-            >
-                <input type="file" name="photos" id="photos" onChange={handleImageChange} multiple />
-            </form>
+
             <div className="button__container">
 
                 <Button
@@ -86,7 +40,7 @@ const PhotosLoadForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => 
                 <Button
                     name="Finish"
                     style="button_dark_small"
-                    onClick={handleFinish}
+                    onClick={() => {}}
                 />
             </div>
         </div>
--- src/components/photoLoader/PhotoLoader.js
+++ src/components/photoLoader/PhotoLoader.js
@@ -1,30 +1,57 @@
-import { React, useEffect } from 'react';
+import { React, useState } from 'react';
 import cameraIcon from '../../assets/icons/camera.svg'
 import './PhotoLoader.css'
 
-const PhotoLoader = ({ photoArray }) => {
+const PhotoLoader = ({userId}) => {
 
-    const photosUploaded = [
-       /*  'https://images.unsplash.com/photo-1601933973783-43cf8a7d4c5f?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-        'https://images.unsplash.com/photo-1612018888057-0c0fef6de098?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80',
-        'https://images.unsplash.com/photo-1611615695859-21c4e8210341?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
-        'https://images.unsplash.com/photo-1611943574537-77f07c3a2aaa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=633&q=80',
-        'https://images.unsplash.com/photo-1611900219970-8b3bbf197e6b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80', */
-    ]
+    const handleImageUpload = (e) => {
+        /* TODO: DEFINE PUT/POST ACTION AGAINST MONGODB */
 
-    useEffect(() => {
-        //setPhotoArray(photosUploaded)
-    }, [])
+        let form_data = new FormData(); // https://developer.mozilla.org/es/docs/Web/API/XMLHttpRequest/FormData
+        form_data.append('updated', new Date());
+        form_data.append('photo', e.target.files[0]);
+
+
+
+        const options = {
+            method: "PUT",
+            body: form_data
+        };
+
+        debugger;
+
+        fetch("http://localhost:3001/api/users/" + userId + "/photos", options)
+            .then((res) => {
+                if (res.ok) {
+                    // fetch de les imatges del user
+                    // .then(respobse => setPhotoArray([respose])) 
+                }
+                else {
+                    console.log("error")
+                }
+            })
+    };
+
+    const [photoArray, setPhotoArray] = useState([]);
 
     const MAX_ALLOWED = 8;
     const photosAllowed = MAX_ALLOWED - photoArray.length
 
     const content = []
-    const photo = `data:${photos.mimeType};base64,${photos.image}`;
+    // const photo = `data:${photo.mimeType};base64,${photo.image}`;
     for (var i = 0; i < photosAllowed; i++) {
         content.push(
             <div className="photoloader__container">
-                <img src={cameraIcon} alt="camera_icon"/>
+                <img src={cameraIcon} alt="camera_icon" />
+                <form id="photo"
+                    encType="multipart/form-data"
+                    className="form__container"
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                    }}
+                >
+                    <input type="file" name="photos" id="photos" onChange={handleImageUpload} />
+                </form>
             </div>
         )
     }
@@ -32,7 +59,7 @@ const PhotoLoader = ({ photoArray }) => {
     return (
         <>{photoArray.map((photo) => {
             return (
-                <img src={photo} alt="uploaded_image" className="photoloader__photouploaded"/>
+                <img src={photo} alt="uploaded_image" className="photoloader__photouploaded" />
             )
         })}
             {content}
