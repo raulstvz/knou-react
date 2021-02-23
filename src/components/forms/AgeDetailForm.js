253100b9b3ae1eeabb4643ad324efa0ecf6aa4a8
Valery <valeryfigueroah@gmail.com>
2021-02-23 19:08:14 +0100

    merge completed

--- src/components/forms/AgeDetailForm.js
+++ src/components/forms/AgeDetailForm.js
@@ -16,6 +16,8 @@ const AgeDetailForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => {
   const body = {
     age: formData.userAge,
     age_range: [formData.ageStart, formData.ageEnd],
+    gender: formData.gender,
+    orientation: formData.orientation,
     signup_step: currentStep + 1,
     signup_completed: false,
     updated: new Date(),
@@ -23,51 +25,131 @@ const AgeDetailForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => {
     //orientation: formData.orientation
   };
 
-    /* Data to be passed as body in the fetch */
-    const body = {
-        age: formData.userAge,
-        age_range: [formData.ageStart, formData.ageEnd],
-        gender: formData.gender,
-        orientation: formData.orientation,
-        signup_step: currentStep + 1,
-        signup_completed: false,
-        updated: new Date(),
-        //gender: formData.gender,
-        //orientation: formData.orientation
-    };
-    fetch("http://localhost:3001/api/users/" + userId, options);
-    setCurrentStep(currentStep + 1);
+/* Actions for the buttons in the forms */
+const handleNext = () => {
+  const options = {
+      method: "PUT",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
   };
+  fetch("http://localhost:3001/api/users/" + userId, options)
+  setCurrentStep(currentStep + 1)
+}
 
-  const handlePrevious = () => {
-    setCurrentStep(currentStep - 1);
-  };
 
-  return (
-    <div className="form">
-      <Stepper steps={totalSteps} currentStep={currentStep} onClick={handleNext} />
-      <form
-        className="form__container"
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-      >
-        <h1>We can't wait to meet you.</h1>
-        <p>Please fill the detail below so that we get to knou you</p>
-        <p>How old are you?</p>
-        <br />
+const handlePrevious = () => {
+  setCurrentStep(currentStep - 1);
+};
+
+return (
+  <div className="form">
+    <Stepper
+      steps={totalSteps}
+      currentStep={currentStep}
+      onClick={handleNext}
+    />
+    <form
+      className="form__container"
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <div className="h1_container">
+        <h2>We can't wait to meet you.</h2>
+      </div>
+      <p>
+        Please fill the detail below so that we get to{" "}
+        <span className="colorPurple">knou</span> you
+      </p>
+      {/* <label className="form_label">Chose your gender </label> */}
+      <div className="form_radio">
+        {/* <label className="form_label">Chose your gender </label> */}
+        <p>Choose your gender</p>
         <input
-          name="source"
-          className="form__input"
-          placeholder="Your Age"
-          onChange={(e) =>
-            setFormData({ ...formData, userAge: parseInt(e.target.value) })
-          }
-          style={{ "max-width": "25%" }}
+          type="radio"
+          id="male"
+          name="gender"
+          value="male"
+          onChange={(e) => setFormData({ ...formData, gender: e.target.value })}
         />
-        <p>What is the age range you are insterested in</p>
+        Male
+        <input
+          type="radio"
+          id="female"
+          name="gender"
+          value="others"
+          onChange={(e) => setFormData({ ...formData, gender: e.target.value })}
+        />
+        Female
+        <input
+          type="radio"
+          id="others"
+          name="gender"
+          value="others"
+          onChange={(e) => setFormData({ ...formData, gender: e.target.value })}
+        />
+        Others
+      </div>
 
-        <div className="slider__container">
+      {/* <label className="form_label">Your sexual orientation</label> */}
+      <div className="form_radio">
+        <p>Choose your sexual orientation</p>
+        <input
+          type="radio"
+          id="heterosexual"
+          name="orientation"
+          value="heterosexual"
+          onChange={(e) =>
+            setFormData({ ...formData, orientation: e.target.value })
+          }
+        />
+        Heterosexual
+        <input
+          type="radio"
+          id="homosexual"
+          name="orientation"
+          value="homosexual"
+          onChange={(e) =>
+            setFormData({ ...formData, orientation: e.target.value })
+          }
+        />
+        Homosexual
+        <input
+          type="radio"
+          id="bisexual"
+          name="orientation"
+          value="bisexual"
+          onChange={(e) =>
+            setFormData({ ...formData, orientation: e.target.value })
+          }
+        />
+        Bisexual
+        <input
+          type="radio"
+          id="transexual"
+          name="orientation"
+          value="transexual"
+          onChange={(e) => setFormData({ ...formData, gender: e.target.value })}
+        />
+        Transexual
+      </div>
+      
+      <p>How old are you?</p>
+      <input
+        name="source"
+        className="form__input"
+        placeholder="Your Age"
+        onChange={(e) =>
+          setFormData({ ...formData, userAge: parseInt(e.target.value) })
+        }
+        style={{ "max-width": "25%" }}
+      />
+
+
+      <p>What is the age range you are insterested in?</p>
+      <div className="slider__container">
           <div className="form_slider_age">
             <div className="value">
               <div className="buble">{formData.ageStart + " years"}</div>
@@ -84,118 +166,22 @@ const AgeDetailForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => {
             />
           </div>
 
-    return (
-        <div className="form">
-            <Stepper steps={totalSteps} currentStep={currentStep} />
-            <form
-                className="form__container"
-                onSubmit={(e) => {
-                    e.preventDefault()
-                }}
-            >
-                <div className="h1_container">
-                <h2>We can't wait to meet you.</h2>
-                </div>
-                <p>Please fill the detail below so that we get to <span className="colorPurple">knou</span> you</p>
-                    {/* <label className="form_label">Chose your gender </label> */}
-                    <p>Choose your gender</p>
-                <div className="form_radio">
-                    <input
-                        type="radio" id="male" name="gender" value="male"
-                            setFormData({ ...formData, gender: e.target.value })
-                        onChange={(e) =>
-                        } />Male
-                    <input type="radio" id="female" name="gender" value="others"
-                        onChange={(e) =>
-                        } />Female
-                            setFormData({ ...formData, gender: e.target.value })
-                    <input type="radio" id="others" name="gender" value="others"
-                        onChange={(e) =>
-                            setFormData({ ...formData, gender: e.target.value })
-                        } />Others
-                </div>
-                <br />
-                <p>How old are you?</p>
-                <input
-                    name="source"
-                    className="form__input"
-                    placeholder="Your Age"
-                    onChange={(e) =>
-                        setFormData({ ...formData, userAge: parseInt(e.target.value) })
-                    }
-                    style={{ "max-width": "25%" }}
-                />
-                <div className="form_radio">
-                    {/* <label className="form_label">Chose your gender </label> */}
-                    <p>Choose your gender</p>
-                    <input
-                        type="radio" id="male" name="gender" value="male"
-                        onChange={(e) =>
-                            setFormData({ ...formData, gender: e.target.value })
-                        } />Male
-                    <input type="radio" id="female" name="gender" value="others"
-                        onChange={(e) =>
-                            setFormData({ ...formData, gender: e.target.value })
-                        } />Female
-
-                    <input type="radio" id="others" name="gender" value="others"
-                        onChange={(e) =>
-                            setFormData({ ...formData, gender: e.target.value })
-                        } />Others
-                </div>
-
-                    {/* <label className="form_label">Your sexual orientation</label> */}
-                <div className="form_radio">
-                    <input
-                    <p>Choose your sexual orientation</p>
-                        type="radio" id="heterosexual" name="orientation" value="heterosexual"
-                        onChange={(e) =>
-                            setFormData({ ...formData, orientation: e.target.value })
-                        } />Heterosexual
-                    <input type="radio" id="homosexual" name="orientation" value="homosexual"
-                        onChange={(e) =>
-                            setFormData({ ...formData, orientation: e.target.value })
-                        } />Homosexual
-                    <input type="radio" id="bisexual" name="orientation" value="bisexual"
-                        onChange={(e) =>
-                            setFormData({ ...formData, orientation: e.target.value })
-                        } />Bisexual
-                     <input type="radio" id="transexual" name="orientation" value="transexual"
-                        onChange={(e) =>
-                            setFormData({ ...formData, gender: e.target.value })
-                </div>
-                        } />Transexual
-
-
-                <div className="button__container">
-                    <Button
-                        name="Next step"
-                        style="button_dark_small"
-                        onClick={handleNext}
-                    />
-                    {currentStep > 1 && <Button
-                        name="Back"
-                        style="button_dark_small"
-                        onClick={handlePrevious} />
-                    }</div>
-                <p>What is the age range you are insterested in?</p>
-                <div className="form__slider">
-                    <input type="range" min="18" max="75" value={formData.ageStart} step="1"
-                        onChange={(e) =>
-                            setFormData({ ...formData, ageStart: parseInt(e.target.value) })
-                    />
-                        }
-                </div>
-                <div className="form__slider">
-                    <input type="range" min="18" max="75" value={formData.ageEnd} step="1"
-                        onChange={(e) =>
-                            setFormData({ ...formData, ageEnd: parseInt(e.target.value) })
-                        }
-                    />
-                </div>
-                <p style={{ "margin-bottom": "50px" }}>You are <b>{formData.userAge} years old</b> and have interest
-                in meeting people <b>between {formData.ageStart} and {formData.ageEnd}</b> years.</p>
-
+          <div className="form_slider_age">
+            <div className="value">
+            <div className="buble">{formData.ageEnd + " years"}</div>
+            </div>
+            <input
+              type="range"
+              min="18"
+              max="75"
+              value={formData.ageEnd}
+              step="1"
+              onChange={(e) =>
+                setFormData({ ...formData, ageEnd: parseInt(e.target.value) })
+              }
+            />
+          </div>
+        </div>
 
         <p style={{ "margin-bottom": "50px" }}>
           You are <b>{formData.userAge} years old</b> and have interest in
@@ -205,16 +191,23 @@ const AgeDetailForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => {
           </b>{" "}
           years.
         </p>
-        <div className="button__container">
+
+      <div className="button__container">
+        <Button
+          name="Next step"
+          style="button_dark_small"
+          onClick={handleNext}
+        />
+        {currentStep > 1 && (
           <Button
-            name="Next step"
+            name="Back"
             style="button_dark_small"
-            onClick={handleNext}
+            onClick={handlePrevious}
           />
-        </div>
-      </form>
-    </div>
-  );
-};
+        )}
+      </div>
+    </form>
+  </div>
+);}
 
 export default AgeDetailForm;
--- src/components/forms/PhotosLoadForm.js
+++ src/components/forms/PhotosLoadForm.js
@@ -11,20 +11,8 @@ const PhotosLoadForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => 
 
     /* Controls the photo insertion through the PhotoLoader component */
 
-
+    
     /* Actions for the buttons in the forms */
-    const handleFinish = () => {
-        /* TODO: DEFINE PUT/POST ACTION AGAINST MONGODB */
-        const options = {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(body),
-        };
-        fetch("http://localhost:3001/api/users/" + userId, options)
-        history.push("/swipePage")
-    };
 
     const handlePrevious = () => {
         setCurrentStep(currentStep - 1)
@@ -33,31 +21,17 @@ const PhotosLoadForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => 
     return (
         <div className="form">
             <Stepper steps={totalSteps} currentStep={currentStep} onClick={handlePrevious}/>
-            <form
-                className="form__container"
-                onSubmit={(e) => {
-                    e.preventDefault()
-                }}
-            >
-                <h1>We can't wait to meet you.</h1>
-                <p>Please fill the detail below so that we get to knou you</p>
-                <br /><br />
-                <p>It's show time! Pick up to 8 pictures of you:</p>
-                <div className="form__photos">
-                    <PhotoLoader setPhotoArray={setPhotoArray} />
-                </div>
-            </form>
-            <div className="button__container">
-
             <h1>We can't wait to meet you.</h1>
             <p>Please fill the detail below so that we get to knou you</p>
             <br /><br />
             <p>It's show time! Pick up to 8 pictures of you:</p>
+
             <div className="form__photos">
                 <PhotoLoader userId={userId}/>
             </div>
-
+        
             <div className="button__container">
+
                 <Button
                     name="Back"
                     style="button_white_small"
@@ -68,7 +42,8 @@ const PhotosLoadForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => 
                     style="button_dark_small"
                     onClick={() => {history.push("swipepage")}}
                 />
-            </div>
+
+                </div>
         </div>
     )
 }
