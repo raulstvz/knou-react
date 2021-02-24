77bc83206444f052025a8dcf45462046b7233693
Raul Alejandro <raul.salcedo03@hotmail.com>
2021-02-24 10:06:41 +0100

    nada

--- src/components/photoLoader/PhotoLoader.js
+++ src/components/photoLoader/PhotoLoader.js
@@ -2,44 +2,35 @@ import { React, useState } from 'react';
 import cameraIcon from '../../assets/icons/camera.svg'
 import './PhotoLoader.css'
 
-const PhotoLoader = ({userId}) => {
+const PhotoLoader = ({ userId }) => {
     const [photoArray, setPhotoArray] = useState([]);
-
     const handleImageUpload = (e) => {
         /* TODO: DEFINE PUT/POST ACTION AGAINST MONGODB */
-
         let form_data = new FormData(); // https://developer.mozilla.org/es/docs/Web/API/XMLHttpRequest/FormData
         form_data.append('updated', new Date());
         form_data.append('photo', e.target.files[0]);
-
         const options = {
             method: "PUT",
-            body: form_data,    
+            body: form_data,
         };
-
-        fetch("http://localhost:3001/api/users/" + userId + "/photos", options)  
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw Error(response.statusText);
-            }
-        })
-        .then((response) => setPhotoArray(response))
-        .catch(error => {
-            console.log("Error when retrieving images:", error);
-        })
-                     
-                    //fetch de les imatges del user
-                    // .then(respobse => setPhotoArray([respose])) 
-                   
+        fetch("http://localhost:3001/api/users/" + userId + "/photos", options)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw Error(response.statusText);
+                }
+            })
+            .then((response) => setPhotoArray(response))
+            .catch(error => {
+                console.log("Error when retrieving images:", error);
+            })
+        //fetch de les imatges del user
+        // .then(respobse => setPhotoArray([respose])) 
     };
-
-   console.log(photoArray);
-
+    console.log(photoArray);
     const MAX_ALLOWED = 8;
     const photosAllowed = MAX_ALLOWED - photoArray.length
-
     const content = []
     // const photo = `data:${photo.mimeType};base64,${photo.image}`;
     for (var i = 0; i < photosAllowed; i++) {
@@ -64,10 +55,9 @@ const PhotoLoader = ({userId}) => {
             return (
                 <img src={src} alt="uploaded_image" className="photoloader__photouploaded" />
             )
-        })} 
-             {content}
+        })}
+            {content}
         </>
     )
 }
-
 export default PhotoLoader
