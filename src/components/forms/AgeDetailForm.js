1d125546e56413f31352647543441e8e742a0abd
Valery <valeryfigueroah@gmail.com>
2021-02-28 23:12:15 +0100

    gender icons added

--- src/components/forms/AgeDetailForm.js
+++ src/components/forms/AgeDetailForm.js
@@ -2,6 +2,10 @@ import { React, useState } from "react";
 import "./Forms.css";
 import Button from "../button/Button";
 import Stepper from "../stepper/Stepper";
+import femaleIcon from "../../assets/gender icons/femaleIcon.svg"
+import maleIcon from "../../assets/gender icons/maleIcon.svg"
+import maleFemaleIcon from "../../assets/gender icons/maleFemaleIcon.svg"
+import orientaSexMaleFemaleIcon from "../../assets/gender icons/orientaSexMaleFemaleIcon.svg"
 
 const AgeDetailForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => {
   const [formData, setFormData] = useState({
@@ -13,7 +17,7 @@ const AgeDetailForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => {
     orientation: undefined,
 
   });
-
+console.log(formData.gender)
   /* Data to be passed as body in the fetch */
   const body = {
     age: formData.userAge,
@@ -43,6 +47,13 @@ const AgeDetailForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => {
     setCurrentStep(currentStep - 1);
   };
 
+  const selectGender = ()=>{
+    alert('you have selected '+ formData.gender) 
+  }
+  const selectOrientation = ()=>{
+    alert ('Your orientation is ' + formData.orientation)
+  }
+
   return (
     <div className="form">
       <Stepper steps={totalSteps} currentStep={currentStep} />
@@ -62,72 +73,91 @@ const AgeDetailForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => {
         <div className="form_radio">
           {/* <label className="form_label">Chose your gender </label> */}
           <p>Choose your gender</p>
+          
+          <label for="male"><img src={maleIcon} alt="male simbol" className="iconGender"/>Male</label>
           <input
             type="radio"
             id="male"
             name="gender"
             value="male"
+            onClick={(selectGender)}
             onChange={(e) =>
               setFormData({ ...formData, gender: e.target.value })
             }
           />
-          Male
+          
+          <label for="female"><img src={femaleIcon} alt="female simbol" className="iconGender"/>Female</label>
           <input
             type="radio"
             id="female"
             name="gender"
             value="female"
+            onClick={(selectGender)}
             onChange={(e) =>
               setFormData({ ...formData, gender: e.target.value })
             }
           />
-          Female
+          
+          <label for="male-female"><img src={maleFemaleIcon} alt="male-female simbol" className="iconGender"/>Male-Female</label>
           <input
             type="radio"
-            id="others"
+            id="male-female"
             name="gender"
-            value="others"
+            value="male-female"
+            onClick={(selectGender)}
             onChange={(e) =>
               setFormData({ ...formData, gender: e.target.value })
             }
           />
-          Others
         </div>
         <br />
         <div className="form_radio">
-          {/* <label className="form_label">Your sexual orientation</label> */}
+      
           <p>Choose your sexual orientation</p>
+
+         
+          <label for="heterosexual"> Heterosexual<img src={femaleIcon} alt="female simbol" className="iconHeteroM"/><img src={maleIcon} alt="male simbol" className="iconHeteroF"/></label>
           <input
             type="radio"
             id="heterosexual"
             name="orientation"
             value="heterosexual"
+            onClick={(selectOrientation)}
             onChange={(e) =>
               setFormData({ ...formData, orientation: e.target.value })
             }
           />
-          Heterosexual
+
+        <label for="homosexual">Homosexual
+        <img src={femaleIcon} alt="female simbol" className="iconHomoFa"/>
+        <img src={femaleIcon} alt="female simbol" className="iconHomoFb"/>
+        <img src={maleIcon} alt="male simbol" className="iconHomoMa"/>
+        <img src={maleIcon} alt="male simbol" className="iconHomoMb"/>
+        </label>
+
           <input
             type="radio"
             id="homosexual"
             name="orientation"
             value="homosexual"
+            onClick={(selectOrientation)}
             onChange={(e) =>
               setFormData({ ...formData, orientation: e.target.value })
             }
           />
-          Homosexual
+
+          <label for="bisexual">Bisexual<img src={orientaSexMaleFemaleIcon} alt="male-female simbol" className="iconGender"/></label>
           <input
             type="radio"
             id="bisexual"
             name="orientation"
             value="bisexual"
+            onClick={(selectOrientation)}
             onChange={(e) =>
               setFormData({ ...formData, orientation: e.target.value })
             }
           />
-          Bisexual
-          <input
+          {/* <input
             type="radio"
             id="transexual"
             name="orientation"
@@ -136,7 +166,7 @@ const AgeDetailForm = ({ totalSteps, currentStep, setCurrentStep, userId }) => {
               setFormData({ ...formData, gender: e.target.value })
             }
           />
-          Transexual
+          Transexual */}
         </div>
 
         <p>How old are you?</p>
