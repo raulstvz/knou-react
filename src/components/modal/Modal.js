78eff98f6ad54f0a05b3d6075f2e93738d8e8174
Franc Cosp Meya <franccospmeyaeso@gmail.com>
2021-03-14 16:23:49 +0100

    Improving in the swiper modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,15 +1,15 @@
-import { React } from "react";
+import React from "react";
 import './Modal.css';
 import Button from "../button/Button";
 
-const Modal = ({ handleClose, visible, content }) => {
+const Modal = ({ handleClose, visible, children }) => {
 
     const showHideClassName = visible ? "modal display-block" : "modal display-none";
 
     return (
     <div className={showHideClassName}>
       <section className="modal__main">
-        {content}
+        {children}
         <Button name="Close" style="button_dark_small" onClick={handleClose} />
       </section>
     </div>
--- src/components/usercard/UserCard.js
+++ src/components/usercard/UserCard.js
@@ -2,19 +2,30 @@ import "./UserCard.css";
 import LikeButton from "../likeAndDislikeButtons/ButtonLike";
 import DislikeButton from "../likeAndDislikeButtons/ButtonDislike";
 import { useEffect, useState } from "react";
+import Modal from "../modal/Modal";
+import CustomCarousel from "../customCarousel/CustomCarousel";
 
 const UserCardReal = ({ possibleMatch, giveLike, giveDislike }) => {
   //implementar logica de Post con likes Y dislikes desde los bot0nes, Aqui sabemos la info de cada usuario.
   const [photo, setPhoto] = useState([]);
+  const [modalVisible,setModalVisible] = useState();
+
+
+  const handleModalClose = () => setModalVisible(false);
+   
 
   useEffect(() => {
-    fetch(`http://localhost:3001/api/photo/${possibleMatch._id}/photos`)
+    fetch(`http://localhost:3001/api/users/${possibleMatch._id}`)
       .then((promise) => {
         if (promise.status === 200) {
           return promise.json();
         }
       })
-      .then((json) => setPhoto(json));
+      .then((json) => {
+        // console.log("flag", parsedJson);
+        console.log("flag2", json)
+        setPhoto(json.photos)
+      });
   }, []);
 
   const photoBuffer = photo.map((e) => {
@@ -28,8 +39,8 @@ const UserCardReal = ({ possibleMatch, giveLike, giveDislike }) => {
 
   return (
     <div className="userCard__container">
-      <div className="profilePicture__container">
-        <img src={photoBuffer} className="profilePicture" />
+      <div className="profilePicture__container" onClick={() => setModalVisible(true)}>
+        <img src={photoBuffer[0]} className="profilePicture" />
       </div>
       <div className="profileInfo__container">
         <p className="profileInfo__nameAndAge">
@@ -47,6 +58,10 @@ const UserCardReal = ({ possibleMatch, giveLike, giveDislike }) => {
         />
         <LikeButton giveLike={giveLike} possibleMatchId={possibleMatch._id} />
       </div>
+      <Modal handleClose={handleModalClose} visible={modalVisible} >
+      tus putos muetod
+        <CustomCarousel photos={photo}/>
+      </Modal>
     </div>
   );
 };
