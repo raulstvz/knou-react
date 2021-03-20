c9d387e27dc2b0f36060ca46d5691f47197fefc3
Franc Cosp Meya <franccospmeyaeso@gmail.com>
2021-03-20 12:11:44 +0100

    change to develop

--- src/hostSettings.js
+++ src/hostSettings.js
@@ -2,7 +2,7 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 if(hostname === 'localhost') {
     backendHost = 'http://127.0.0.1:3001';
-} else if(hostname === '') {
+} else if(hostname === 'hardcore-hawking-033be2.netlify.app') {
     backendHost = 'https://knou-node-d3u6i.ondigitalocean.app/';
 }
 else backendHost = 'http://127.0.0.1:3001';
