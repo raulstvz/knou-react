9986d0c085932f3a480d6263197313b96f912fda
Franc Cosp Meya <franccospmeyaeso@gmail.com>
2021-03-20 19:18:16 +0100

    merging

--- src/utils/hostSettings.js
+++ src/utils/hostSettings.js
@@ -1,8 +1,8 @@
 let backendHost;
 const hostname = window && window.location && window.location.hostname;
-if(hostname === 'localhost') {
+if (hostname === 'localhost') {
     backendHost = 'http://127.0.0.1:3001';
-} else if(hostname === 'hardcore-hawking-033be2.netlify.app') {
+} else if (hostname === 'hardcore-hawking-033be2.netlify.app') {
     backendHost = 'https://knou-node-d3u6i.ondigitalocean.app';
 }
 else backendHost = 'http://127.0.0.1:3001';
