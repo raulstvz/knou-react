227e207a9dafda6e5a85771d81b01572e3bd3dcc
Valery <valeryfigueroah@gmail.com>
2021-03-20 10:26:58 +0100

    modal added

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -7,26 +7,33 @@ import React, { useEffect, useState } from "react";
 import Footer from "../components/footer/Footer";
 import Tag from "../components/tag/Tag";
 import tagIcon from '../assets/icons/tag.svg'
+import Modal from "../components/modal/Modal"
 
 const UserProfilePage = () => {
   const [age, setAge] = useState('');
 
   const [errorStyle, setErrorStyle] = useState({
     'age': 'errorInvisible',
-    // 'gender': 'errorInvisible',
-    // 'orientation': 'errorInvisible'
-
-
+    'description' : 'errorInvisible',
+    'hobbies' : 'errorInvisible'
   });
+
 const history = useHistory();
-const [profile, setProfile] = useState([]);
+const [photo, setPhoto] = useState([]);
+const [modalVisible, setModalVisible] = useState();
+const handleModalClose = ()=> setModalVisible(false);
+const [profile, setProfile] = useState();
+console.log(profile)
 const [description, setDescription] = useState('')
 const [placeholder, setPlaceHolder] = useState("");
 const [tagArray, setTagArray] = useState([]);
+const [refresh, setRefresh] = useState(false);
+
 const [formData, setFormData] = useState({
   hobbies: undefined,
   ageStart: 25,
-  ageEnd: 35,});
+  ageEnd: 35
+});
 const user = JSON.parse(localStorage.getItem("user")); 
 console.log(user.lastname); // prueba si funciona
 
@@ -48,10 +55,14 @@ const updateInfo = () => {
     fetch(`http://localhost:3001/api/users/${user._id}`,options)
       .then((response) => {
         if (response.status === 200) {
-        }
+          return response.json();
+        } 
       }
       )
-
+      .then((json) => {
+        localStorage.setItem("user", JSON.stringify(json))
+        setProfile(json)
+      });
 };
 
 const handleKeyPress = (e) => {
@@ -61,153 +72,187 @@ const handleKeyPress = (e) => {
     e.target.value = placeholder;
   }
 };
+
+useEffect(() => {
+  setFormData({ ...formData, hobbies: tagArray });
+}, [tagArray]);
+
 const handleOnClick = (e) => {
   let tagName = e.target.nextSibling.textContent;
   setTagArray(tagArray.filter((tag) => tag !== tagName));
 };
 
 
-// useEffect(() => {
-//   fetch(`http://localhost:3001/api/profile${user._id}`)
-//     .then((promise) => {
-//       if (promise.status === 200) {
-//         return promise.json();
-//       }
-//     })
-//     .then((json) => setProfile(json));
-// }, []);
+useEffect(() => {
+  fetch(`http://localhost:3001/api/users/${user._id}`)
+    .then((promise) => {
+      if (promise.status === 200) {
+        return promise.json();
+      }
+    })
+    .then((json) => setProfile(json));
+}, []);
 
 
+//fetch de photos.
+useEffect(() => {
+  fetch(`http://localhost:3001/api/photo/${user._id}/photos`)
+    .then((promise) => {
+      if (promise.status === 200) {
+        return promise.json();
+      }
+    })
+    .then((json) => setPhoto(json));
+}, []);
+
+const photoBuffer = photo.map((e) => {
+  const src = `data:${e.mimeType};base64,${Buffer.from(e.photo.data).toString(
+    "base64"
+  )}`;
+  return src;
+});
 
 return (
-    <div className="userProfile__container">
-    <Header
-        icons={<BoxMenu />}
-        button={
-        <Button
-            name="Log out"
-            onClick={() => history.push("/")}
-            style="button_white_small"
+  <>
+  {profile &&
+    <div className="userProfile__container">  
+        <Header
+            icons={<BoxMenu />}
+            button={
+            <Button
+                name="Log out"
+                onClick={() => history.push("/")}
+                style="button_white_small"
+            />
+            }
         />
-        }
-    />
-    <h2 className="title_editProfile">Edit your profile</h2>
-    <div className="currentInfo_container">
-        <div className="nonTouchableInfo_container">
-            <div className="nonTouchableInfo_section">
-              <p>Your full name is {user.firstname}  {user.lastname}</p>
-              <p>your email is <br></br>{user.email}</p>
-              <p>Your age is {user.age}</p>
-              <p>Your gender is {user.gender}</p>
-              <p>Your sex orientation is <br></br>{user.orientation}</p>
+      <h2 className="title_editProfile">Edit your profile</h2>
+        <div className="currentInfo_container">
+            
+            <div className="nonTouchableInfo_container">
+                <div className="nonTouchableInfo_section">
+                  <p>Your full name is {profile.firstname}  {profile.lastname}</p>
+                  <p>your email is <br></br>{profile.email}</p>
+                  <p>Your age is {profile.age}</p>
+                  <p>Your gender is {profile.gender}</p>
+                  <p>Your sex orientation is <br></br>{profile.orientation}</p>
+                </div>
+
+                <div className="profilePicture__container">
+                  <img src={photoBuffer[0]} className="profilePicture"  onClick={()=>setModalVisible(true)} />
+                </div>
+                <Modal handleClose={handleModalClose}
+          visible={modalVisible}
+          children={<div>hola</div>}>
+</Modal>
             </div>
-            <div><img src={user.photos[1]}/>Aquí va la foto,aunque no se renderiza</div>
-        </div>
-        <div className="touchableInfo_container">
-          <div className="touchableInfo_section">
-              <p>Your age range interest is:</p>
-                <div className="currentTouchableAgeRange_container">
-                  <p>{user.age_range[0]}</p>
-                  <p>{user.age_range[1]}</p>
+            <div className="touchableInfo_container">
+              <div className="touchableInfo_section">
+                  <p>Your age range interest is:</p>
+                    <div className="currentTouchableAgeRange_container">
+                      <p>{profile.age_range[0]}</p>
+                      <p>{profile.age_range[1]}</p>
+                    </div>
+                  
+                  <p>Your hobbies are:
+                  <div className="tag_profile_container">
+                  <div className="tag__container__created">
+                            <p>{profile.hobbies[0]}</p>
+                            <img id="tagIcon" src={tagIcon} alt="tag_icon" />
+                    </div>
+                    <div className="tag__container__created">
+                            <p>{profile.hobbies[1]}</p>
+                            <img id="tagIcon" src={tagIcon} alt="tag_icon" />
+                    </div>
+                    <div className="tag__container__created">
+                            <p>{user.hobbies[2]}</p>
+                            <img id="tagIcon" src={tagIcon} alt="tag_icon" />
+                    </div>
+                    <div className="tag__container__created">
+                            <p>{user.hobbies[3]}</p>
+                            <img id="tagIcon" src={tagIcon} alt="tag_icon" />
+                    </div>
+                    <div className="tag__container__created">
+                            <p>{user.hobbies[4]}</p>
+                            <img id="tagIcon" src={tagIcon} alt="tag_icon" />
+                    </div>
+                  </div>
+                  </p>
+                  <p>Description <br></br>{user.description}</p>
                 </div>
-              
-              <p>Your hobbies are:
-              <div className="tag_profile_container">
-              <div className="tag__container__created">
-                        <p>{user.hobbies[0]}</p>
-                        <img id="tagIcon" src={tagIcon} alt="tag_icon" />
+              <div className="newTouchableInfo_container">
+                <div >
+                  <p className="newInfoTitle">Update your new profile!</p>
+                  
+                <div className="slider__container">
+                  <div className="form_slider_age">
+                <div className="value">
+                  <div className="buble_profile">{formData.ageStart + " years"}</div>
                 </div>
-                <div className="tag__container__created">
-                        <p>{user.hobbies[1]}</p>
-                        <img id="tagIcon" src={tagIcon} alt="tag_icon" />
-                </div>
-                <div className="tag__container__created">
-                        <p>{user.hobbies[2]}</p>
-                        <img id="tagIcon" src={tagIcon} alt="tag_icon" />
-                </div>
-                <div className="tag__container__created">
-                        <p>{user.hobbies[3]}</p>
-                        <img id="tagIcon" src={tagIcon} alt="tag_icon" />
-                </div>
-                <div className="tag__container__created">
-                        <p>{user.hobbies[4]}</p>
-                        <img id="tagIcon" src={tagIcon} alt="tag_icon" />
-                </div>
+                <input
+                  type="range"
+                  min="18"
+                  max="75"
+                  value={formData.ageStart}
+                  step="1"
+                  onChange={(e) =>
+                    setFormData({ ...formData, ageStart: parseInt(e.target.value) })
+                  }
+                />
               </div>
-              </p>
-              <p>Description <br></br>{user.description}</p>
-            </div>
-
-
-          <div className="newTouchableInfo_container">
-            <div >
-              <p className="newInfoTitle">Update your new profile!</p>
-              
-            <div className="slider__container">
               <div className="form_slider_age">
-            <div className="value">
-              <div className="buble">{formData.ageStart + " years"}</div>
+                <div className="value">
+                  <div className="buble_profile">{formData.ageEnd + " years"}</div>
+                </div>
+                <input
+                  type="range"
+                  min="18"
+                  max="75"
+                  value={formData.ageEnd}
+                  step="1"
+                  onChange={(e) =>
+                    setFormData({ ...formData, ageEnd: parseInt(e.target.value) })
+                  }
+                />
+              </div>
             </div>
-            <input
-              type="range"
-              min="18"
-              max="75"
-              value={formData.ageStart}
-              step="1"
-              onChange={(e) =>
-                setFormData({ ...formData, ageStart: parseInt(e.target.value) })
-              }
-            />
-          </div>
-          <div className="form_slider_age">
-            <div className="value">
-              <div className="buble">{formData.ageEnd + " years"}</div>
+
+            <div >
+              <Tag
+                tagArray={tagArray}
+                onKeyPress={handleKeyPress}
+                placeholder={placeholder}
+                onClick={handleOnClick} 
+                />
             </div>
-            <input
-              type="range"
-              min="18"
-              max="75"
-              value={formData.ageEnd}
-              step="1"
-              onChange={(e) =>
-                setFormData({ ...formData, ageEnd: parseInt(e.target.value) })
-              }
+
+            <textarea
+              className="textarea_container"
+              placeholder="Your new description..."
+              rows="10"
+              onChange={(e) =>{
+                setDescription(e.target.value)
+              console.log(description)}}
             />
-          </div>
-        </div>
-
-        <div >
-          <Tag
-            tagArray={tagArray}
-            onKeyPress={handleKeyPress}
-            placeholder={placeholder}
-            onClick={handleOnClick} 
-            />
-        </div>
-
-        <textarea
-          className="textarea_container"
-          placeholder="Your new description..."
-          rows="10"
-          onChange={(e) =>{
-            setDescription(e.target.value)
-          console.log(description)}}
-        />
-
-
-
+                <p>your last update was {user.updated}</p>
+                </div>
+              </div>   
             </div>
-          </div>   
-        </div>
-        <div className="button_update">
-          <Button
-            name="Update Info"
-            style="button_dark_small"
-            onClick={updateInfo}
-          /></div>
+            <div className="button_update">
+              <Button
+                name="Update Info"
+                style="button_dark_small"
+                onClick={updateInfo}
+              /></div>
+        </div> 
+
+
+
+
+        <Footer />
     </div>
-    <Footer />
-    </div>
+}
+    </>
 );
 };
 
