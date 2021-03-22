7ba6c6a6779d24e55241105a076585ea58d9634e
Franc Cosp Meya <franccospmeyaeso@gmail.com>
2021-03-22 09:36:46 +0100

    last changes

--- src/utils/hostSettings.js
+++ src/utils/hostSettings.js
@@ -3,8 +3,8 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === 'localhost') {
     backendHost = 'http://127.0.0.1:3001';
 } else if(hostname === 'hardcore-hawking-033be2.netlify.app') {
-    backendHost = 'https://knou-node-d3u6i.ondigitalocean.app';
+    backendHost = 'https://knou-node-d3u6i.ondigitalocean.app/';
 }
-else backendHost = 'http://127.0.0.1:3001';
+else backendHost = 'http://127.0.0.1:3001/';
 export const API_ROOT = `${backendHost}`;
 export const HOSTNAME = `${hostname}`;
